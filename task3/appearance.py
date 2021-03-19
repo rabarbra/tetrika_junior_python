0bb4b2bdcd793c128cb45230e551ab2c69ac0a54
polina <rabarbrablad@gmail.com>
2021-03-19 16:25:10 +0300

    Added sorting for arrays of intervals

--- task3/appearance.py
+++ task3/appearance.py
@@ -1,25 +1,32 @@
 #-*- encoding: utf-8 -*-
 
+def sort_by_begin(arr):
+    decorated = [(arr[i], arr[i + 1]) for i in range(0, len(arr), 2)]
+    decorated.sort()
+    return(decorated)
+
 def intersect_interval_arrays(arr1, arr2):
     i = 0
     j = 0
     result = []
-    while i + 1 < len(arr1) and j + 1 < len(arr2):
-        if arr2[j] >= arr1[i + 1]:
-            i += 2
-        elif arr1[i] >= arr2[j + 1]:
-            j += 2
+    arr1 = sort_by_begin(arr1)
+    arr2 = sort_by_begin(arr2)
+    while i < len(arr1) and j < len(arr2):
+        if arr2[j][0] >= arr1[i][1]:
+            i += 1
+        elif arr1[i][0] >= arr2[j][1]:
+            j += 1
         else:
-            if arr1[i] < arr2[j]:
-                result.append(arr2[j])
+            if arr1[i][0] < arr2[j][0]:
+                result.append(arr2[j][0])
             else:
-                result.append(arr1[i])
-            if arr1[i + 1] < arr2[j + 1]:
-                result.append(arr1[i + 1])
-                i += 2
+                result.append(arr1[i][0])
+            if arr1[i][1] < arr2[j][1]:
+                result.append(arr1[i][1])
+                i += 1
             else:
-                result.append(arr2[j + 1])
-                j += 2
+                result.append(arr2[j][1])
+                j += 1
     return(result)
 
 
